6a57d3484621a46e7194caee7de875431e844d1e
DougT13 <dougfs2004@gmail.com>
2022-09-19 20:17:54 -0300

    imagem funcionando

--- captcha.php
+++ captcha.php
@@ -1,9 +1,10 @@
 <?php
+
    session_start(); // inicial a sessao
    header("Content-type: image/jpeg"); // define o tipo do arquivo
 
-    function captcha($largura,$altura,$tamanho_fonte,$quantidade_letras){
-        $imagem = imagecreate($largura,$altura); // define a largura e a altura da imagem
+    function captcha($larguraInt,$alturaInt,$tamanho_fonte,$quantidade_letras){
+        $imagem = imagecreate($larguraInt,$alturaInt); // define a largura e a altura da imagem
         $fonte = getcwd()."/arial.ttf"; //voce deve ter essa ou outra fonte de sua preferencia em sua pasta
         $preto  = imagecolorallocate($imagem,0,0,0); // define a cor preta
         $branco = imagecolorallocate($imagem,255,255,255); // define a cor branca
@@ -20,11 +21,16 @@
         imagejpeg($imagem); // gera a imagem
         imagedestroy($imagem); // limpa a imagem da memoria
     }
+    
 
-    $largura = $_GET["l"]; // recebe a largura
-    $altura = $_GET["a"]; // recebe a altura
-    $tamanho_fonte = $_GET["tf"]; // recebe o tamanho da fonte
-    $quantidade_letras = $_GET["ql"]; // recebe a quantidade de letras que o captcha terá
+    $largura = $_POST['l']; // recebe a largura
+    $altura = $_POST['a']; // recebe a altura
+    $larguraInt = intval($largura);
+    $alturaInt = intval($altura);
+    $tamanho_fonte = 20; // recebe o tamanho da fonte
+    $quantidade_letras = 5; // recebe a quantidade de letras que o captcha terá
+
+
     captcha($largura,$altura,$tamanho_fonte,$quantidade_letras);
     
 ?>
--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 	<title>Captcha</title>
 </head>
 <body>
-	<img src="captcha.php?l=150&a=50&tf=20&ql=5">
 
-	<form action="validar.php" name="form" method="post">
-   		<input type="text" name="palavra"  />
+	<form action="captcha.php" name="form" method="get">
+   		<input type="number" id="a" name="a" value="a"  placeholder="altura" />
+   		<input type="number" id="l" name="l" value="l"  placeholder="largura"/>
    		<input type="submit" value="Validar Captcha" />
 	</form>
 </body>
